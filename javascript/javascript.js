3b09c1c6d02728ae222780625f5f19c2958621ed
celestegalarza <celestevalentinagalarza@gmail.com>
2025-01-17 00:47:21 -0300

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,25 @@
+
+
+function cambiarfondo1() {document.body.style.backgroundColor = "black";
+document.getElementById("section").style.color= "whitesmoke";
+document.getElementById("nav").style.backgroundImage= "none";
+document.getElementById("redessociales").style.color = "black";
+}
+
+
+function cambiarfondo2() {
+  document.body.style.backgroundColor = "lightblue";
+}
+
+function cartel() {alert("Potrero Digital")}
+
+function piel() {
+  document.getElementById("infor").innerHTML = "detalles de la rosacea";
+}
+
+function colorojo() {
+  document.getElementById("red").style.color = "red";
+}
+function creditos() { alert("Diseño hecho por Celeste Galarza - correo electronico: celestevalentinagalarza@gmail.com")
+
+}
